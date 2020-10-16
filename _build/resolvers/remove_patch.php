fb125ed41d12e7c8d784c33a3158033e7705933d
Ilya Utkin <ilyautkin@mail.ru>
2020-10-16 14:30:30 +0300

    Fix XSS in TV Access Permissions and resource list TV

--- _build/resolvers/remove_patch.php
+++ _build/resolvers/remove_patch.php
@@ -1,7 +1,7 @@
 <?php
 if (strpos(__DIR__, 's24006') === false) {
-    unlink(MODX_CORE_PATH . 'packages/modx-2.8.0-patch-1.0.0-pl.transport.zip');
+    unlink(MODX_CORE_PATH . 'packages/modx-2.8.0-patch-1.0.1-pl.transport.zip');
     if ($cache = $transport->xpdo->getCacheManager()) {
-        $cache->deleteTree(MODX_CORE_PATH . 'packages/modx-2.8.0-patch-1.0.0-pl/', ['deleteTop' => true, 'skipDirs' => false, 'extensions' => []]);
+        $cache->deleteTree(MODX_CORE_PATH . 'packages/modx-2.8.0-patch-1.0.1-pl/', ['deleteTop' => true, 'skipDirs' => false, 'extensions' => []]);
     }
 }
